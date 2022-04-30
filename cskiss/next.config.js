c5d8efa1245b1e136253ef08e2fd446cf17b94d2
kanjurer <kanavb9999@gmail.com>
2022-04-30 20:57:10 -0300

    understood nextjs and basic setup

--- cskiss/next.config.js
+++ cskiss/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const path = require("path");
+
+const nextConfig = {
+    reactStrictMode: true,
+};
+
+module.exports = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+    },
+    nextConfig,
+};
